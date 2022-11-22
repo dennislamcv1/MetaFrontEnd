e67c892cfbf0d09d4249ee848099cb0bd8e77400
Dennis Lam <39684447+dennislamcv1@users.noreply.github.com>
2022-11-22 10:31:10 +0800

    Add files via upload

---
+++ Advanced React/Create your portfolio/src/components/LandingSection.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
+import FullScreenSection from "./FullScreenSection";
+
+const greeting = "Hello, I am Pete!";
+const bio1 = "A frontend developer";
+const bio2 = "specialised in React";
+
+// Implement the UI for the LandingSection component according to the instructions.
+// Use a combination of Avatar, Heading and VStack components.
+const LandingSection = () => (
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+
+  </FullScreenSection>
+);
+
+export default LandingSection;
---
+++ Advanced React/basic List/src/DessertsList.js
@@ -0,0 +1,23 @@
+function DessertsList(props) {
+  // Implement the component here.
+const DessertsList = (props) => {
+ const lowCaloriesDesserts = props.data
+   .filter((dessert) => {
+     return dessert.calories < 500;
+   })
+   .sort((a, b) => { 
+     return a.calories - b.calories; 
+   })
+   .map((dessert) => { 
+     return ( 
+       <li>
+         {dessert.name} - {dessert.calories} cal 
+       </li> 
+     ); 
+   }); 
+ return <ul>{lowCaloriesDesserts}</ul>; 
+
+}
+}
+
+export default DessertsList;
